f17a6198e6eae4bcbf47f954ce17fa4fb884aa51
Emma66000 <ingargio@etud.insa-toulouse.fr>
2022-01-25 16:43:55 +0100

    affichage du prenom

--- screens/Chapter1.js
+++ screens/Chapter1.js
@@ -1,22 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
+import react from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, TextInput } from "react-native";
 
 const Chapter1 = function() {
+  const [prenom, onChangeText] = react.useState(null);
   return (
-    <View style={styles.container}>
-      <Text>First chapter</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={prenom}
+        placeholder="Your firstname"
+        keyboardType="default"
+      />
+      <Text>{prenom}</Text>
+    </SafeAreaView>
   );
 }
 
 export {Chapter1};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
