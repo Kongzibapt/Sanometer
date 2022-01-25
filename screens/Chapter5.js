9a0104f864f0230bbba55f10056e33b88922007e
Carinou <carinamalleville@gmail.com>
2022-01-25 16:22:18 +0100

    Ajout trame chapter5

---
+++ screens/Chapter5.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Chapter5() {
+  return (
+    <>  <View style={styles.container}>
+            <Text>Fifth chapter</Text>
+            <StatusBar style="auto" />
+        </View>
+        <View style={styles.container}>
+            <Text>Measurements</Text>
+            <StatusBar style="auto" />
+        </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
