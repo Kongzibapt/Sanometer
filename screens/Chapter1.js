0adfd0e6f12a3391637951a8ac27942a8c558bad
Emma66000 <ingargio@etud.insa-toulouse.fr>
2022-01-25 15:31:57 +0100

    first screen

--- screens/Chapter1.js
+++ screens/Chapter1.js
@@ -1,18 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {Chapter1} from './screens/Chapter1.js';
 
-export default function App() {
+export default function Chapter1() {
   return (
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-      
-    //   <StatusBar style="auto" />
-    // </View>
-    <View>
-      <Chapter1/>
+    <View style={styles.container}>
+      <Text>First chapter</Text>
+      <StatusBar style="auto" />
     </View>
-    
   );
 }
 
