420308d3e1a775e0e5e195d10774ae3da2fe90ae
Carinou <carinamalleville@gmail.com>
2022-03-22 15:09:02 +0100

    fonction imc fin

--- utils/function_Carina.js
+++ utils/function_Carina.js
@@ -1,14 +1,12 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const IMCAdvices = () => {
-    const weight = await AsyncStorage.getItem('poids');
-    const height = await AsyncStorage.getItem('taille');
+    const weight = await AsyncStorage.getItem('weight');
+    const height = await AsyncStorage.getItem('height');
 
     var imc = weight / (height*height);
-    console.log(imc);
-    result="";
+    var result="";
     if (imc < 18.9){
         result = "Malnutrition : URGENT : medical advice needed";
     }
-    console.log(result);
 }
