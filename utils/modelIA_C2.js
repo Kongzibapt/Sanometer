09d1379ade4483d3293813b57c1a13e9810d2731
Enairo4 <orianeberry4@gmail.com>
2022-04-22 13:56:09 +0200

    fin tab chap2 et avancement tab parametres physio

--- utils/modelIA_C2.js
+++ utils/modelIA_C2.js
@@ -4,13 +4,28 @@ export const chapter2 = async () => {
 
     
     const allergies = await AsyncStorage.getItem('checked_allergiesante');
+    const severefood = await AsyncStorage.getItem('checked_severefood');
+    const severedrugs = await AsyncStorage.getItem('checked_severedrugs');
+    const severeseason = await AsyncStorage.getItem('checked_severeseason');
+    const severeothers = await AsyncStorage.getItem('checked_severeothers');
+    const vaccines = await AsyncStorage.getItem('checked_vax');
 
     var tab = [];
 
     if (allergies == 'no') {
         tab[0] = 0;
+        tab[1] = 0;
     } else if (allergies == 'yes') {
         tab[0] = 1;
+        if (severefood == 'yes' || severedrugs == 'yes' || severeseason== 'yes' || severeothers == 'yes'){
+            tab[1]=1;
+        }
+    }
+
+    if (vaccines == 'yes') {
+        tab[2] = 1;
+    } else {
+        tab[2] = 0;
     }
 
     console.log(tab);
